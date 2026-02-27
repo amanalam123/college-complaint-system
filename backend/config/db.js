5f0ad5ce2cc4784d6ec5e75ab1aaa06b3c20239a
Aman Alam <amanalamdumri123@gmail.com>
2026-02-27 18:49:44 +0530

    Final deployment version

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 5000,
+    });
+
     console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.error("MongoDB Connection Failed:", error.message);
